54a8dc42bd84daad5c0f1bcb22fd4f7610a9e950
PhilaniNcube <ncbphi001@gmail.com>
2026-08-10 20:55:04 +0200

    feat: implement root layout with SEO metadata, analytics, and site structure components

--- src/app/(frontend)/layout.tsx
+++ src/app/(frontend)/layout.tsx
@@ -69,6 +69,9 @@ export const metadata: Metadata = {
   icons: {
     icon: "/favicon.ico",
   },
+  verification: {
+    google: "AU65mtbDbkyasySGHc5WaGLFgUjKCWx35M9yRgUnCjM",
+  },
 };
 
 export default function RootLayout({
